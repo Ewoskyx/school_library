78f0fffbd8d09a0da1d96cd7342b92d21ac702eb
Ewoskyx <ewoskyx@gmail.com>
2022-03-02 02:47:55 +0300

    Remove comments

--- corrector.rb
+++ corrector.rb
@@ -1,10 +1,7 @@
 class Corrector
-  # def correct_name(name)
-  #   correct_name = name.capitalize
-  #   correct_name = name.slice(0, 10) if name.size > 10
-  #   correct_name
-  # end
   def correct_name(name)
-    name.gsub(name[0], name[0].upcase)[0, 10]
+    correct_name = name.capitalize
+    correct_name = name.slice(0, 10) if name.size > 10
+    correct_name
   end
 end
