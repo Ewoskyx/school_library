18143fd8062209ab408065941eeec4b88eb0fc7b
Ewoskyx <ewoskyx@gmail.com>
2022-03-02 02:42:00 +0300

    Add relations

--- student.rb
+++ student.rb
@@ -1,11 +1,14 @@
-require './person'
+require_relative './person'
+
 class Student < Person
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
-    @classroom = classroom
-  end
+  attr_reader :classroom
 
   def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
